a1b325154193d887329c42f9dfd007dd6b50b45c
qemcx <qemc.gf@gmail.com>
2022-09-23 14:34:24 +0200

    navbar update

---
+++ src/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+import './Button.css';
+
+const STYLES = ['btn--primary', 'btn--primary'];
+const SIZES = ['btn--medium', 'btn--large', 'btn--mobile']
+const COLOR = ['primary', 'blue', 'red', 'green']
+
