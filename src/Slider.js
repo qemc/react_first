3b1d2f8218504654e462184b624f5ab5c65450e2
qemcx <qemc.gf@gmail.com>
2022-09-24 15:53:05 +0200

    slider updade

--- src/Slider.js
+++ src/Slider.js
@@ -1,17 +1,47 @@
 import React from "react";
 import './Slider.css'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Slider = ({slides}) => {
 
     const [currentIndex, setCurrentIndex] = useState(0);
 
+    const handleLeftArrow = () => {
+        
+        if(currentIndex > 0){
+            setCurrentIndex(currentIndex - 1);
+        }
+        if(currentIndex === 0){
+            setCurrentIndex(4);
+        }
+    }
 
+    const handleRightArrow = () => {
+        
+        if(currentIndex < 4 && currentIndex !== 4){
+            setCurrentIndex(currentIndex + 1 );
+        }
+        if(currentIndex === 4){
+            setCurrentIndex(0);
+        }
+    }
+
+    
+
+    const handleDotClick = (e) =>{
+        const currentObj = e.target;
+        setCurrentIndex(currentObj.getAttribute('value'))
+
+        console.log(`url(${slides[currentIndex].url})`)
+    }
+    
     const sliderStyle = {
         height: '100%',
         position: 'relative',
+        
     }
     const slideStyles = {
+        transition: '0.4s',
         height:'100%',
         borderRadius: '10px',
         backgroundPosition: 'center',
@@ -21,24 +51,32 @@ const Slider = ({slides}) => {
         backgroundColor:'green',
         backgroundImage: `url(${slides[currentIndex].url})`,
     }
-    const leftArrowStyles = {
-        position: 'absolute',
-        top: '40%',
-        fontSize:'50px'
-    }
-    const rightArrowStyles = {
-        position: 'absolute',
-        top: '40%',
-        left: '99%',
-    }
 
+
+
+    // useEffect(()=>{
+
+    //     console.log('dziala')
+
+
+    // }, [currentIndex])
+
+    
     return ( 
         <div style={sliderStyle}>
-            <div style={leftArrowStyles}>❰</div>
-            <div style={rightArrowStyles}>❱</div>
+            <div className="arrow-container">
+                <div onClick={handleLeftArrow} className="left arrow"></div>
+                <div className="dots">
+                    {slides.map((slide, slideIndex)=>(
+                        <div key={slideIndex} onClick={()=>setCurrentIndex(slideIndex)} className="dot"></div>
+                    ))}
+                </div>
+                <div onClick={handleRightArrow} className="right arrow"></div>
+            </div>
             <div style={slideStyles}>
             </div>
         </div>
+        
      );
 }
 
